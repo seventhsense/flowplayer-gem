fd671a23aba26a511b2da1eb946122b3d9d043b0
Scott Davis <jetviper21@gmail.com>
2011-11-08 19:12:12 -0500

    derp

--- lib/flowplayer.rb
+++ lib/flowplayer.rb
@@ -1,5 +1,8 @@
 require 'flowplayer/player'
 
 if defined?(Rails)
+  if Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 1
+    require 'flowplayer/engine'
+  end
   require 'flowplayer/railtie'
 end
