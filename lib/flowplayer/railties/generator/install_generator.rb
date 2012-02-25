0c813000170e18bc612d773948fedc1df8c28c56
Sebastian Spieszko <ciasteczkowy@gmail.com>
2012-02-25 16:38:28 +0100

    dodges generator issue described at https://github.com/scottdavis/flowplayer-gem/issues/2

--- lib/flowplayer/railties/generator/install_generator.rb
+++ lib/flowplayer/railties/generator/install_generator.rb
@@ -1,24 +1,22 @@
-module Flowplayer
-  module Generator
-    class InstallGenerator < Rails::Generators::Base
-      namespace 'flowplayer'
-      source_root File.expand_path("../../../../../vendor/assets/", __FILE__)
-      argument :install_type, :type => :string, :banner => "commercial", :required => false, :default => ''
-      
-      def install_flowplayer
-        copy_file 'javascripts/flowplayer-3.2.4.min.js', Rails.root.join('public', 'javascripts', 'flowplayer.min.js')
-      end
-      
-      def install_swfs
-        if install_type == 'commercial'
-          copy_file 'flash/flowplayer.commercial-3.2.5.swf', Rails.root.join('public', 'flowplayer.swf')
-        else
-          copy_file 'flash/flowplayer-3.2.5.swf', Rails.root.join('public', 'flowplayer.swf')
-          copy_file '../../FLOWPLAYER_LICENSE.txt', Rails.root.join('FLOWPLAYER_LICENSE.txt')
-        end
-        copy_file 'flash/flowplayer.controls-3.2.3.swf', Rails.root.join('public', 'flowplayer.controls.swf')
-      end
-      
+module Flowplayer::Generator
+  class InstallGenerator < Rails::Generators::Base
+    namespace 'flowplayer'
+    source_root File.expand_path("../../../../../vendor/assets/", __FILE__)
+    argument :install_type, :type => :string, :banner => "commercial", :required => false, :default => ''
+    
+    def install_flowplayer
+      copy_file 'javascripts/flowplayer-3.2.4.min.js', Rails.root.join('public', 'javascripts', 'flowplayer.min.js')
     end
+    
+    def install_swfs
+      if install_type == 'commercial'
+        copy_file 'flash/flowplayer.commercial-3.2.5.swf', Rails.root.join('public', 'flowplayer.swf')
+      else
+        copy_file 'flash/flowplayer-3.2.5.swf', Rails.root.join('public', 'flowplayer.swf')
+        copy_file '../../FLOWPLAYER_LICENSE.txt', Rails.root.join('FLOWPLAYER_LICENSE.txt')
+      end
+      copy_file 'flash/flowplayer.controls-3.2.3.swf', Rails.root.join('public', 'flowplayer.controls.swf')
+    end
+    
   end
 end
